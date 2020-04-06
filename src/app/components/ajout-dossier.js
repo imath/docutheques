c70f28ec40f24174f8c41115d145501ac2f407f8
imath <imathi.eu@outlook.fr>
2020-04-06 15:45:10 +0200

    Start building the dossier form

--- src/app/components/ajout-dossier.js
+++ src/app/components/ajout-dossier.js
@@ -2,19 +2,71 @@
  * WordPress dependencies
  */
 const { Component, createElement } = wp.element;
+const { TextControl, TextareaControl, Button } = wp.components;
+const { dispatch } = wp.data;
 const { __ } = wp.i18n;
+
+const DEFAULT_STATE = {
+	name: '',
+	description: '',
+	parent: 0,
+}
 
 class DocuthequesDossierForm extends Component {
 	constructor() {
 		super( ...arguments );
+
+		this.state = DEFAULT_STATE;
+
+		this.resetState = this.resetState.bind( this );
+	}
+
+	resetState() {
+		this.setState( DEFAULT_STATE );
+	}
+
+	closeForm( e ) {
+		e.preventDefault();
+
+		this.resetState();
+		dispatch( 'docutheques' ).setCurrentState( 'documentsBrowser' );
+	}
+
+	submitForm( e ) {
+		e.preventDefault();
 	}
 
 	render() {
 		const { user, dossier } = this.props;
+		const { name, description } = this.state;
 
 		return (
 			<div className="formulaire-dossier">
-				{ __( 'Formulaire d’envoi de dossiers.', 'docutheques' ) }
+				<button className="close dashicons dashicons-no" onClick={ ( e ) => this.closeForm( e ) }>
+					<span className="screen-reader-text">{ __( 'Fermer le formulaire d’ajout de dossier', 'docutheques' ) }</span>
+				</button>
+
+				<h2 className="title">{ __( 'Créer un nouveau dossier', 'docutheques' ) }</h2>
+
+				<TextControl
+					label={ __( 'Nom du dossier (obligatoire)', 'docutheques' ) }
+					value={ name }
+					onChange={ ( name ) => this.setState( { name: name } ) }
+				/>
+
+				<TextareaControl
+					label={ __( 'Description', 'docutheques' ) }
+					value={ description }
+					onChange={ ( description ) => this.setState( { description: description } ) }
+				/>
+
+				<Button isLarge={ true } onClick={ ( e ) => this.closeForm( e ) }>
+					{ __( 'Annuler', 'docutheques' ) }
+				</Button>
+
+				<Button isPrimary={ true } isLarge={ true } onClick={ ( e ) => this.submitForm( e ) }>
+					{ __( 'Ajouter', 'docutheques' ) }
+				</Button>
 			</div>
 		);
 	}
