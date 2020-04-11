fee001a8dedbd2864a37d641e74aadc23aa09bee
imath <math.viet@gmail.com>
2020-04-11 19:30:04 +0200

    Add a modal to confirm dossier deletion

--- src/app/components/barre-outils.js
+++ src/app/components/barre-outils.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 const { Component, Fragment, createElement } = wp.element;
-const { Button } = wp.components;
+const { Button, Modal } = wp.components;
 const { withSelect, dispatch } = wp.data;
 const { compose } = wp.compose;
 const { __ } = wp.i18n;
@@ -10,6 +10,15 @@ const { __ } = wp.i18n;
 class DocuthequesToolbar extends Component {
 	constructor() {
 		super( ...arguments );
+
+		this.state = {
+			isDeleteDossierModalOpen: false,
+			isDeleteDocumentModalOpen: false,
+		};
+
+		this.openDossierModal = this.openDossierModal.bind( this );
+		this.closeDossierModal = this.closeDossierModal.bind( this );
+		this.deleteDossier = this.deleteDossier.bind( this );
 	}
 
 	switchMode( e, mode ) {
@@ -18,8 +27,25 @@ class DocuthequesToolbar extends Component {
 		dispatch( 'docutheques' ).switchMode( mode );
 	}
 
+	openDossierModal() {
+		this.setState( { isDeleteDossierModalOpen: true } );
+	}
+
+	closeDossierModal() {
+		this.setState( { isDeleteDossierModalOpen: false } );
+	}
+
+	deleteDossier() {
+		const { dossier } = this.props;
+
+		// @todo handle Dossier delete.
+
+		this.setState( { isDeleteDossierModalOpen: false } );
+	}
+
 	render() {
 		const { dossier, isAdvancedEditMode } = this.props;
+		const { isDeleteDossierModalOpen } = this.state;
 		const documentsSelection = 0;
 		let gridClass = 'view-grid';
 		let avancedEditClass = 'view-list';
@@ -50,9 +76,30 @@ class DocuthequesToolbar extends Component {
 							<Button isLarge={ true } className="button media-button select-mode-toggle-button">
 								{ __( 'Modifier le dossier actif', 'docutheques' ) }
 							</Button>
-							<Button isLarge={ true } className="button media-button select-mode-toggle-button">
+							<Button isLarge={ true } className="button media-button select-mode-toggle-button" onClick={ this.openDossierModal }>
 								{ __( 'Supprimer le dossier actif', 'docutheques' ) }
 							</Button>
+							{ isDeleteDossierModalOpen && (
+								<Modal
+									title={ __( 'Suppression du dossier actif', 'docutheques' ) }
+									onRequestClose={ this.closeDossierModal }
+									className="delete-dossier-confirmation"
+								>
+
+									{ __( 'Cette action supprimera tous les éventuels dossiers et documents contenus dans ce dossier, vous confirmez ?', 'docutheques' ) }
+
+									<div className="confirmation-buttons">
+										<Button isLarge={ false } isPrimary={ true } onClick={ this.deleteDossier }>
+											{ __( 'Supprimer', 'docutheques' ) }
+										</Button>
+
+										<Button isLarge={ false } isSecondary={ true } onClick={ this.closeDossierModal }>
+											{ __( 'Annuler', 'docutheques' ) }
+										</Button>
+									</div>
+
+								</Modal>
+							) }
 						</Fragment>
 					) }
 				</div>
